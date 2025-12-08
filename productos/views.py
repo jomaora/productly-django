a4f356a18fcb3f525582f8e94fb184c0b2a7a0bc
Joan Ortega <jomaora@gmail.com>
2025-12-08 08:19:30 +0100

    Add product details

--- productos/views.py
+++ productos/views.py
@@ -5,3 +5,7 @@ def index(request):
   productos = Producto.objects.select_related('categoria').all()
   print(productos)
   return render(request, 'index.html', context={'productos': productos})
+
+def detalle(request, producto_id):
+  producto = Producto.objects.select_related('categoria').get(id=producto_id)
+  return render(request, 'detalle.html', context={'producto': producto})
