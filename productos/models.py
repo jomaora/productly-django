ecd13bf3f10b11f4b297b636127c273ff3b884a4
Joan Ortega <jomaora@gmail.com>
2025-12-08 08:02:21 +0100

    Adding template

--- productos/models.py
+++ productos/models.py
@@ -4,8 +4,9 @@ from django.utils import timezone
 # Create your models here.
 class Categoria(models.Model):
   nombre = models.CharField(max_length=255)
-  
+
   def __str__(self) -> str:
+    print(self.nombre)
     return str(self.nombre)
 
 class Producto(models.Model):
--- productos/views.py
+++ productos/views.py
@@ -1,7 +1,7 @@
-from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import Producto
 
 def index(request):
-  productos = Producto.objects.all().values()
-  return JsonResponse(list(productos), safe=False)
+  productos = Producto.objects.select_related('categoria').all()
+  print(productos)
+  return render(request, 'index.html', context={'productos': productos})
