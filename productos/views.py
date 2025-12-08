589aa6cb616716e44faff38924c5961f2a900306
Joan Ortega <jomaora@gmail.com>
2025-12-08 08:25:48 +0100

    with get_object_or_404

--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Producto
 
 def index(request):
@@ -8,9 +7,5 @@ def index(request):
   return render(request, 'index.html', context={'productos': productos})
 
 def detalle(request, producto_id):
-  try:
-    producto = Producto.objects.select_related('categoria').get(id=producto_id)
-    return render(request, 'detalle.html', context={'producto': producto})
-  except Producto.DoesNotExist:
-    raise Http404("Producto no encontrado")
-  
+  producto = get_object_or_404(Producto, id=producto_id)
+  return render(request, 'detalle.html', context={'producto': producto})
